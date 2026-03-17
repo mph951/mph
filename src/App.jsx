6d742a8195c489e469c8a3195adc32c21a4b4a33
mph951 <mphradio951@gmail.com>
2026-03-17 15:16:43 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1115,7 +1115,7 @@ function MainApp({ onLogout }) {
 }
 
 /* ─── CONFIG ── */
-const GAS_URL = "https://script.google.com/macros/s/AKfycbx4CafPbrXpyQO60Ub2hCvWyG6ZVT0U8JDIvzRMLeXPgCg_W9wxCGW53EVlpXwdZIJ9/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbz2SKc0dzhL_Auy4KUhgI9v3BkenGWtgVHBoTzH6Mp1Uj7NqYK4VNgQUrkupoGkFzUU/exec";
 const ACCESS_PASSWORD = "mph951";
 
 export default function App() {
